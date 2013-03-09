ddb5f7bb014b35e3c456f2dc8c2880a368b88a3d
dewski <me@garrettbjerkhoel.com>
2013-03-09 13:17:21 -0800

    Add some Glimpse::Views::Git tests

--- test/glimpse/views/git_test.rb
+++ test/glimpse/views/git_test.rb
@@ -1,13 +1,33 @@
 require 'test_helper'
 
 describe Glimpse::Views::Git do
-  before do
-    @git = Glimpse::Views::Git.new(:nwo => 'github/test', :sha => '123')
-  end
-
   describe "compare url" do
+    before do
+      @git = Glimpse::Views::Git.new(:nwo => 'github/test', :sha => '123')
+    end
+
     it "should return the full url" do
       assert_equal 'https://github.com/github/test/compare/master...123', @git.compare_url
+    end
+  end
+
+  describe "sha" do
+    before do
+      @git = Glimpse::Views::Git.new(:sha => '123')
+    end
+
+    it "should return correct sha" do
+      assert_equal '123', @git.sha
+    end
+  end
+
+  describe "branch name" do
+    before do
+      @git = Glimpse::Views::Git.new(:sha => '123', :branch_name => 'glimpse')
+    end
+
+    it "should return correct branch name" do
+      assert_equal 'glimpse', @git.branch_name
     end
   end
 end
