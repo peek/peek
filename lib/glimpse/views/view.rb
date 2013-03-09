6375a918ea9a18be432862f7e00e5f3aeacad803
dewski <me@garrettbjerkhoel.com>
2013-03-09 00:27:42 -0800

    Add a enabled? flag that can be overriden

--- lib/glimpse/views/view.rb
+++ lib/glimpse/views/view.rb
@@ -1,6 +1,14 @@
 module Glimpse
   module Views
     class View
+      def initialize(options = {})
+        @options = options
+      end
+
+      def enabled?
+        true
+      end
+
       def partial_path
         "glimpse/views/#{self.class.to_s.split('::').last.downcase}"
       end
--- test/glimpse/views/view_test.rb
+++ test/glimpse/views/view_test.rb
@@ -1,10 +1,19 @@
 require 'test_helper'
 
 describe Glimpse::Views::View do
+  before do
+    @view = Glimpse::Views::View.new
+  end
+
   describe "partial path" do
     it "should return correct partial class" do
-      @view = Glimpse::Views::View.new
-      assert_equal 'views/view', @view.partial_path
+      assert_equal 'glimpse/views/view', @view.partial_path
+    end
+  end
+
+  describe "toggling off and on" do
+    it "should be enabled by default" do
+      assert @view.enabled?
     end
   end
 end
