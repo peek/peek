4e325b5a623228a236c0efe922f94dab86a62b22
dewski <me@garrettbjerkhoel.com>
2013-03-09 12:23:18 -0800

    Use a railtie to setup all Glimpse views

--- lib/glimpse/railtie.rb
+++ lib/glimpse/railtie.rb
@@ -1,5 +1,11 @@
 module Glimpse
   class Railtie < ::Rails::Engine
     config.glimpse = Glimpse
+
+    initializer 'glimpse.setup_subscribers' do
+      ActiveSupport.on_load(:after_initialize) do
+        Glimpse.views
+      end
+    end
   end
 end
