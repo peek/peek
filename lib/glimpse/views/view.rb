45f7216010dbe74cf39f1013c67a2dceb146df2b
dewski <me@garrettbjerkhoel.com>
2013-03-09 13:32:12 -0800

    Conform to a single setup subscribers method

--- lib/glimpse/views/view.rb
+++ lib/glimpse/views/view.rb
@@ -3,6 +3,8 @@ module Glimpse
     class View
       def initialize(options = {})
         @options = options
+
+        setup_subscribers
       end
 
       def enabled?
@@ -25,6 +27,12 @@ module Glimpse
         ActiveSupport::Notifications.subscribe(*args) do |name, start, finish, id, payload|
           yield name, start, finish, id, payload
         end
+      end
+
+      private
+
+      def setup_subscribers
+        # pass
       end
     end
   end
