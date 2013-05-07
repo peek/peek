25517b60660080e7c2069d22dab032dd278265c8
dewski <me@garrettbjerkhoel.com>
2013-05-06 20:58:50 -0700

    Add memcache adapter

---
+++ lib/peek/adapters/memcached.rb
@@ -0,0 +1,21 @@
+require 'peek/adapters/base'
+require 'dalli'
+
+module Peek
+  module Adapters
+    class Memcache < Base
+      def initialize(options = {})
+        @client = options.fetch(:client, Dalli::Client.new)
+        @expires_in = options.fetch(:expires_in, 60 * 30)
+      end
+
+      def get(request_id)
+        @client.get("peek:requests:#{request_id}")
+      end
+
+      def save
+        @client.add("peek:requests:#{Peek.request_id}", Peek.results.to_json, @expires_in)
+      end
+    end
+  end
+end
