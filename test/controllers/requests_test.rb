451a4ea09c95fe78f46b2adff1391256a2e9d447
Chris Ewald <chrisewald@gmail.com>
2015-10-12 20:33:39 -0400

    add tests for the peek cookie

--- test/controllers/requests_test.rb
+++ test/controllers/requests_test.rb
@@ -1,14 +1,30 @@
 require 'test_helper'
 
 class RequestsTest < ActionDispatch::IntegrationTest
+  include ActiveSupport::Testing::MethodCallAssertions
+
   setup do
     Peek.adapter.reset
     Peek.reset
   end
 
-  test "the request id is set" do
+  test "the request id is set when the peek cookie is set" do
     assert_empty Peek.adapter.requests
+    cookies['peek'] = 'true'
     get '/'
     assert_not_empty Peek.adapter.requests
+  end
+
+  test "saves results if the peek cookie is set" do
+    cookies['peek'] = 'true'
+    assert_called(Peek.adapter, :save) do
+      get '/'
+    end
+  end
+
+  test "does not save results if the peek cookie is not set" do
+    assert_not_called(Peek.adapter, :save) do
+      get '/'
+    end
   end
 end
