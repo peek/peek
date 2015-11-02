fe42812d264c2eab1922043b98516b95d745f56b
Chris Ewald <chrisewald@gmail.com>
2015-11-02 11:58:32 -0500

    set the peek cookie by default in tests

--- test/controllers/requests_test.rb
+++ test/controllers/requests_test.rb
@@ -6,23 +6,23 @@ class RequestsTest < ActionDispatch::IntegrationTest
   setup do
     Peek.adapter.reset
     Peek.reset
+    cookies['peek'] = 'true'
   end
 
   test "the request id is set when the peek cookie is set" do
     assert_empty Peek.adapter.requests
-    cookies['peek'] = 'true'
     get '/'
     assert_not_empty Peek.adapter.requests
   end
 
   test "saves results if the peek cookie is set" do
-    cookies['peek'] = 'true'
     assert_called(Peek.adapter, :save) do
       get '/'
     end
   end
 
   test "does not save results if the peek cookie is not set" do
+    cookies.delete('peek')
     assert_not_called(Peek.adapter, :save) do
       get '/'
     end
