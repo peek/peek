ab19cd21b0a26f747a8a62f84c877c310bea204d
Chris Ewald <chrisewald@gmail.com>
2015-11-02 12:07:29 -0500

    add assertions for cookie values

--- test/controllers/requests_test.rb
+++ test/controllers/requests_test.rb
@@ -17,12 +17,14 @@ class RequestsTest < ActionDispatch::IntegrationTest
 
   test "saves results if the peek cookie is set" do
     get '/'
+    assert_equal cookies['peek'], 'true'
     assert_not_nil Peek.adapter.get(request.env['action_dispatch.request_id'])
   end
 
   test "does not save results if the peek cookie is not set" do
     cookies.delete('peek')
     get '/'
+    assert_nil cookies['peek']
     assert_nil Peek.adapter.get(request.env['action_dispatch.request_id'])
   end
 end
