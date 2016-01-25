414dc226cf481754c2a0fe63402f8809af0c006a
Mikhail Grachev <work@mgrachev.com>
2016-01-25 14:53:39 +0300

    Remove deprecation warnings
    Use method prepend_before_action instead prepend_before_filter and
    request.env instead env

--- lib/peek/controller_helpers.rb
+++ lib/peek/controller_helpers.rb
@@ -3,14 +3,14 @@ module Peek
     extend ActiveSupport::Concern
 
     included do
-      prepend_before_filter :set_peek_request_id, :if => :peek_enabled?
+      prepend_before_action :set_peek_request_id, :if => :peek_enabled?
       helper_method :peek_enabled?
     end
 
     protected
 
     def set_peek_request_id
-      Peek.request_id = env['action_dispatch.request_id']
+      Peek.request_id = request.env['action_dispatch.request_id']
     end
 
     def peek_enabled?
